8edb43f871653a4165129e5eecd589c64c145702
Enamel Systems <enamelsystems@gmail.com>
2013-12-23 13:50:14 +0900

    Add ESBeacon.[ch] files.

---
+++ ESBeacon/ESBeacon.h
@@ -0,0 +1,13 @@
+//
+//  ESBeacon.h
+//  ESBeacon
+//
+//  Created by Enamel Systems on 2013/12/23.
+//  Copyright (c) 2014 Enamel Systems. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ESBeacon : NSObject
+
+@end
---
+++ ESBeacon/ESBeacon.m
@@ -0,0 +1,13 @@
+//
+//  ESBeacon.m
+//  ESBeacon
+//
+//  Created by Enamel Systems on 2013/12/23.
+//  Copyright (c) 2014 Enamel Systems. All rights reserved.
+//
+
+#import "ESBeacon.h"
+
+@implementation ESBeacon
+
+@end
